863992e595dc2408a4b9c6468861d99c8b39c4c9
Nikita <pro100.vce@gmail.com>
2019-02-11 01:28:37 +0300

    Cart in header - interactive

---
+++ media/plugins.js
@@ -0,0 +1,18 @@
+/**
+*  Ajax Autocomplete for jQuery, version 1.4.7
+*  (c) 2017 Tomas Kirda
+*
+*  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.
+*  For details, see the web site: https://github.com/devbridge/jQuery-Autocomplete
+*/
+
+// ==================================================
+// fancyBox v3.3.5
+//
+// Licensed GPLv3 for open source use
+// or fancyBox Commercial License for commercial use
+//
+// http://fancyapps.com/fancybox/
+// Copyright 2018 fancyApps
+//
+// ==================================================
